48a904c2de61b3fff145f37d60759a85278d34c6
Shizophrenia993 <tiynov.you@yandex.ru>
2024-05-15 23:30:12 +0300

    create page auth with template jinja2

---
+++ src/apps/users/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+from src.apps.users.forms import LoginForm, RegisterForm
+from src.apps.users.models import User
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            print(user)
+            if user is not None:
+                login(request, user)
+                print("logged in") # TODO redirect to index
+            else:
+                return render(request, 'login.html', {'error': 'Неверное имя пользователя или пароль'})
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            User.objects.create_user(**form.cleaned_data)
+            return redirect("login")
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
